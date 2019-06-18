4917b3aa949e27cf04a44bad92f5a228cb4d9e49
Corentin <corentin.berteaux@student-cs.fr>
2019-06-18 15:06:16 +0200

    Create notation table

---
+++ server/src/models/notation.py
@@ -0,0 +1,32 @@
+"""
+Define the Notation model
+"""
+from . import db
+from .abc import BaseModel, MetaBaseModel
+from .movie import Movie
+from .user import User
+
+
+class Notation(db.Model, BaseModel, metaclass=MetaBaseModel):
+    """ The Notation model """
+
+    __tablename__ = "notation"
+
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Integer, primary_key=False)
+
+    movie_title = db.Column(db.Integer, db.ForeignKey('movie.title'))
+    movie = db.relationship("Movie", back_populates="notation")
+
+    user_firstname = db.Column(db.Integer, db.ForeignKey('user.firstname'))
+    user_lastname = db.Column(db.Integer, db.ForeignKey('user.lastname'))
+    user = db.relationship("User", back_populates="notation")
+    
+
+    def __init__(self, id, value, movie_title, user_firstname, user_lastname):
+        """ Create a new User """
+        self.id = id
+        self.value = value
+        self.user_firstname = user_firstname
+        self.user_lastname = user_lastname
+        self.movie_title = movie_title
