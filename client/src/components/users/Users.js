0dbcb98883b50a80ed69e86701aae5245c6c50d7
Corentin <corentin.berteaux@student-cs.fr>
2019-06-19 15:18:45 +0200

    Mise en forme users

--- client/src/components/users/Users.js
+++ client/src/components/users/Users.js
@@ -4,54 +4,30 @@ import './Users.css'
 
 
 function Users () {
- return(
- <div className="user">
-
-<div className="user1">   
+    return(
+        <div className="user">
+            <div className="title">   
                 <h1>Mon compte</h1>
-        </div>
+            </div>
 
+            <div class="column-layout">
+    
+                <div className="align">
+                    <h2>Informations utilisateur : </h2>
 
+                    <h4>nom utilisateur : jean</h4>
+                    <h4>age : 12</h4>
+                </div> 
 
-  <div class="column-layout">
-        
+                <div className="align">
+                    <h2>Mes notes</h2>
 
-   <div>     
-        <div className="user2">
-            <h2>Informations utilisateur : </h2>
-        </div>
-
-
-        <div className="user3">
-            <h4>nom utilisateur : jean</h4>
-            <h4>age : 12</h4>
-        </div>
-   </div> 
-   <div>
-        <div className="user4">
-            <h2>Mes notes</h2>
-        </div>    
-
-        <div className="user5">
-            <h4>hihi : 4/5</h4>                                 
-            <h4>star wars 2/5</h4>
-   </div>        
-  </div>
- </div>
-                
-
-
-                 
-                   
-        
-
-                     
-
-         
-      </div>
-  
+                    <h4>hihi : 4/5</h4>                                 
+                    <h4>star wars 2/5</h4>
+                </div>        
+            </div>
+        </div>  
     )
-  
   }
 
 export default Users;
