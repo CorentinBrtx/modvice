09ec662bb81df4125b27fc6e977ff541c8896e4d
Dim 's <dimitri.diximus@student-cs.fr>
2019-06-19 15:19:50 +0200

    création page home

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Prompt } from "react-router-dom";
-
+import Home from './components/home/Home.js'
 import Users from './components/users/Users.js'
+
 
 function App() {
   return (
     <Router>
+      <Route path='/home' component={Home} />
       <Route path='/user' component={Users} />
+      
+
     </Router>
   );
 }
---
+++ client/src/components/home/Home.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Home.css'
+
+
+
+function Home () {
+ return(
+ <div className="user">
+
+   <div className="user1">   
+                <h1>Home</h1>
+        </div>
+
+
+
+  <div class="column-layout">
+        
+
+   <div>     
+        <div className="user2">
+            <h2>Pour vous : </h2>
+        </div>
+
+
+        <div className="user3">
+            <h4>hihi</h4>
+            <h4>star wars</h4>
+        </div>
+   </div> 
+   <div>
+        <div className="user4">
+            <h2>Populaires</h2>
+        </div>    
+
+          <div className="user5">
+            <h4>coucou</h4>                                 
+            <h4>corentin is big </h4>
+          </div>        
+   </div>
+  </div> 
+ </div>
+
+
+ )
+}
+
+export default Home;
