8b910f725ab2a11daf8ad0963821ca1c7a338284
Dim 's <dimitri.diximus@student-cs.fr>
2019-06-20 18:06:19 +0200

    lier user avec 5000

--- client/src/components/users/Users.js
+++ client/src/components/users/Users.js
@@ -4,7 +4,9 @@ import './Users.css';
 import superagent from 'superagent';
 
 
-function Users ({match}) {var notes = null;
+function Users ({match}) {
+    
+    var notes = null;
 
     const [user, setUser] = React.useState(null);
 
@@ -15,12 +17,7 @@ function Users ({match}) {var notes = null;
          .then(response => setUser(response.body.user));
      }, []);
 
-     React.useEffect(() => {
-        superagent
-        .get("http://localhost:5000/application/Notation/user/"+match.params.username)
-        .then(response => {notes = response.body.notations;
-                            notes = notes.map((note) => [{title: note.movie_title, note: note.value}] );});
-        }, []);
+
 
 
         console.log(user)
@@ -37,15 +34,6 @@ function Users ({match}) {var notes = null;
             <div className="Infos">
                 <ul><li>Age {user.age}</li></ul>
             </div>
-
-            <div>
-                <h2>vos notes : </h2>
-            </div>
-
-            <div>
-                <ul> {notes.map(note => ("<li>"+ note.title+ ":" + note.note +"/10"+ "</li>"))} </ul>
-            </div>
-
 
         </div>
 
